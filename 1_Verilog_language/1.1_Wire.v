9e115df15996d6b47d6672cd00f78fd671941803
juneisheng <38127296+juneisheng@users.noreply.github.com>
2021-05-15 15:36:35 +0900

    Update 1.1_Wire.v

--- 1_Verilog_language/1.1_Wire.v
+++ 1_Verilog_language/1.1_Wire.v
@@ -1,5 +1,6 @@
 // A continuous assignment assigns the right side to the left side continuously, so any change to the RHS is immediately seen in the LHS.
 module top_module( input in, output out );
+  
   assign out = in;
 
 endmodule
