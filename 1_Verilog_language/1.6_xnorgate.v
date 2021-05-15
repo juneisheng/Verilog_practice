4d1f34111bff8c8e1f2223239ffd30768babbfb9
juneisheng <38127296+juneisheng@users.noreply.github.com>
2021-05-15 16:54:45 +0900

    Update 1.6_xnorgate.v

--- 1_Verilog_language/1.6_xnorgate.v
+++ 1_Verilog_language/1.6_xnorgate.v
@@ -1,4 +1,7 @@
 // The bitwise-XOR operator is ^. There is no logical-XOR operator.
+// 同或门，输入相同时 输出为1
+// bitwise多输入多输出，logical多输入单输出
+// bitwise operator多输入时，输入数据为vector
 
 module top_module( 
   input a, 
