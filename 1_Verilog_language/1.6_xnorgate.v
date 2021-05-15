b01b7348d4f75e5704cfd7b3cfb8491ca5fb6540
juneisheng <38127296+juneisheng@users.noreply.github.com>
2021-05-15 17:00:28 +0900

    Update 1.6_xnorgate.v

--- 1_Verilog_language/1.6_xnorgate.v
+++ 1_Verilog_language/1.6_xnorgate.v
@@ -1,5 +1,5 @@
 // The bitwise-XOR operator is ^. There is no logical-XOR operator.
-// 同或门，输入相同时 输出为1
+// XNOR同或，输入相同时 输出为1,异或门 串联 非门
 // bitwise多输入多输出，logical多输入单输出
 // bitwise operator多输入时，输入数据为vector
 
