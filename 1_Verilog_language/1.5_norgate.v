8ae7fc4229a9861e53ea041889e7339a7c38748e
juneisheng <38127296+juneisheng@users.noreply.github.com>
2021-05-15 16:51:16 +0900

    Update 1.5_norgate.v

--- 1_Verilog_language/1.5_norgate.v
+++ 1_Verilog_language/1.5_norgate.v
@@ -1,11 +1,12 @@
 // Verilog has separate bitwise-OR (|) and logical-OR (||) operators, like C. 
 // Since we're working with a one-bit here, it doesn't matter which we choose.
+// 或非，或门 串联 非门
 
 module top_module( 
   input a, 
   input b, 
   output out );
   
-  assign out = a | b;
+  assign out = ~(a | b);
 
 endmodule
