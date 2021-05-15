66b4a19baf35cd3a11eac70403160a61e4c7dd8e
juneisheng <38127296+juneisheng@users.noreply.github.com>
2021-05-15 15:37:22 +0900

    Update 1.3_notgate.v

--- 1_Verilog_language/1.3_notgate.v
+++ 1_Verilog_language/1.3_notgate.v
@@ -1,5 +1,7 @@
 // Verilog has separate bitwise-NOT (~) and logical-NOT (!) operators, like C. 
 // Since we're working with a one-bit here, it doesn't matter which we choose.
 module top_module( input in, output out );
+  
   assign out = ~in;
+  
 endmodule
