fabd035a1712ecf54e10b106f0739a00811c23fa
juneisheng <38127296+juneisheng@users.noreply.github.com>
2021-05-15 18:06:02 +0900

    Rename 1.37-gates100.v to 1.37_gates100.v

---
+++ 1_Verilog_language/1.38_vector100r.v
@@ -0,0 +1,10 @@
+module top_module( 
+  input [99:0] in,
+  output [99:0] out
+);
+  always @(*) begin
+    for (int i = 0; i < $bits(in); i++) begin
+      out[i] = in[$bits(in) - 1 - i];
+    end  
+  end
+endmodule
